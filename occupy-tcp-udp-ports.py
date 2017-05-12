5b2d5896df1c0f69b70d1f77251f1bdd3f853a2e
lastmind4@mini <lastmind4@gmail.com>
2017-05-12 09:20:25 +0800

    Polish: refactor & add exception handling

--- occupy-tcp-udp-ports.py
+++ occupy-tcp-udp-ports.py
@@ -2,17 +2,38 @@
 import socket
 import time
 
-def main():
+def occupy_ports():
   bindings = []
+  fail_ports = []
 
   # Occupy port range from 2000 to 2999
   for port in range(2000, 3000):
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp.bind(('', port))
-    tcp.listen(1)
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp.bind(('', port))
+    try:
+      tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      tcp.bind(('', port))
+      tcp.listen(1)
+    except:
+      fail_ports.append(('tcp', port))
+
+    try:
+      udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      udp.bind(('', port))
+    except:
+      fail_ports.append(('udp', port))
     bindings.extend([tcp, udp])
+
+  return (bindings, fail_ports)
+
+def print_fail_ports(fail_ports):
+  if len(fail_ports) == 0:
+    return
+  print 'Failed to bind these ports:'
+  for port_info in fail_ports:
+    print port_info
+
+def main():
+  bindings, fail_ports = occupy_ports()
+  print_fail_ports(fail_ports)
 
   # Pause program
   # Plan A, Wait for user input
