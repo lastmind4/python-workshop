f71e02bc55de99a8f04864d512ed4da39e7af681
lastmind4@mini <lastmind4@gmail.com>
2017-05-12 09:10:07 +0800

    Polish: add other method to pause program, use __main__

--- occupy-tcp-udp-ports.py
+++ occupy-tcp-udp-ports.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 import socket
+import time
 
-bindings = []
+def main():
+  bindings = []
 
-# Occupy port range from 2000 to 2999
-for port in range(2000, 3000):
-  tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  tcp.bind(('', port))
-  tcp.listen(1)
-  udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  udp.bind(('', port))
-  bindings.extend([tcp, udp])
+  # Occupy port range from 2000 to 2999
+  for port in range(2000, 3000):
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.bind(('', port))
+    tcp.listen(1)
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp.bind(('', port))
+    bindings.extend([tcp, udp])
 
-# Wait for user input
-raw_input()
+  # Pause program
+  # Plan A, Wait for user input
+  raw_input()
+  # Plan B, sleep 3600 seconds
+  # time.sleep(3600)
+
+if __name__ == "__main__":
+  main()
